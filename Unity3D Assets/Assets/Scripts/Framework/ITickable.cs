940dde6e2b5ae4d90cace60b2b4bd7745d825e9c
weien02 <weien02@gmail.com>
2026-09-20 19:16:57 +0800

    feature: add stress and consistency test for DedicatedServer

--- Unity3D Assets/Assets/Scripts/Framework/ITickable.cs
+++
@@ -1,7 +0,0 @@
-namespace DedicatedServer.Framework
-{
-    public interface ITickable
-    {
-        void Tick(float deltaTime);
-    }
-}
--- Unity3D Assets/Assets/Scripts/Unity/ECS/Data/DataIndex.cs
+++
@@ -1,7 +0,0 @@
-namespace DedicatedServer.UnityFramework.ECS.Data
-{
-    public enum DataIndex : ushort
-    {
-        TransformData,
-    }
-}
